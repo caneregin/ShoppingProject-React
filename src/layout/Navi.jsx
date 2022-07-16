4bdb7cd898069be19d934a1e6865edea57097466
caneregin <36435160+caneregin@users.noreply.github.com>
2022-07-17 01:51:01 +0300

    productDetail navbar eklendi

--- src/layout/Navi.jsx
+++ src/layout/Navi.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import CartSummary from './CartSummary'
-import { Container, Menu } from 'semantic-ui-react'
+import { Container, Menu, Input } from 'semantic-ui-react'
 import SignedOut from './SignedOut'
 import SignedIn from './SignedIn'
 import { useSelector } from 'react-redux'
@@ -8,6 +8,7 @@ import { useHistory } from 'react-router-dom'
 import { useDispatch } from 'react-redux';
 import { changeToCategoryToggle } from '../store/actions/changeCategoryToggleActions';
 import "../App.css"
+import ProductService from '../services/productService'
 
 export default function Navi() {
     let history = useHistory();
@@ -44,10 +45,33 @@ export default function Navi() {
     const handleChangeToBizeUlasin = () => {
         history.push("/")
     }
+    const [products, setProducts] = useState([]);
+    let changeArray = []
+    useEffect(() => {
+        let productService = new ProductService()
+        productService.getProductWithCategoryDetails().then(result => setProducts(result.data.data))
+
+    }, changeArray)
+    const [text, setText] = useState("")
+    const [suggestions, setSuggestions] = useState([])
+    const onChangeHandler = (text) => {
+        let matches = []
+        if (text.length > 0) {
+            matches = products.filter(
+                product => {
+                    const regex = new RegExp(`${text}`, "gi")
+                    return product.productName.match(regex)
+                }
+            )
+        }
+        console.log('matches', matches)
+        setSuggestions(matches)
+        setText(text)
+    }
     return (
         <div>
 
-            <Menu>
+            <Menu className='altindex'>
                 <Container>
                     <Menu.Item style={{ marginLeft: 160 }} className="menutextaligncenter" onClick={() => handleChangeToWebeOzel()}
                         name="'Web'e Özel'" />
@@ -69,8 +93,18 @@ export default function Navi() {
                     <Menu.Item className="menutextaligncenter" onClick={() => handleChangeToBizeUlasin()}
                         name='Bize Ulaşın!' />
                 </Container></Menu>
-            <Menu style={{marginTop:-10}}>
+            <Menu className='altindex' style={{ marginTop: -10 }}>
                 <Container><img onClick={goHome} src="logo.png" style={{ height: "3rem", width: "9.57rem" }} />
+                    <Menu.Menu position='right'>
+                        <div className='autocomplete'>
+                        <Input icon='search' type="text" style={{ marginTop:2, width:500 }}
+                                onChange={e => onChangeHandler(e.target.value)}
+                                value={text} placeholder='Search...' />
+                            
+                            <div className='autocomplete-items'>
+                                {suggestions && suggestions.map((suggestion, i) =>
+                                    <div key={i} className='suggestion col-md-12 justify-content-md-center'>{suggestion.productName}</div>)}
+                            </div> </div> </Menu.Menu>
                     <Menu.Menu position='right'>
                         {cartItems.length > 0 && <CartSummary />}
                         {localStorage.getItem("currentUser") != null ? <SignedIn /> : <SignedOut />}
--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
-import { Image, Grid, Header, Label, Button, Icon, Card, Menu } from 'semantic-ui-react'
+import { Grid, Header, Label, Button, Icon, Card, Form, TextArea } from 'semantic-ui-react'
 import ProductService from '../services/productService'
 import { addToCart } from "../store/actions/cartActions"
 import { useDispatch, useSelector } from 'react-redux'
@@ -9,7 +9,6 @@ import { Link } from 'react-router-dom';
 import Carousel from "react-elastic-carousel"
 import CardCarousel from './CardCarousel'
 import "../App.css"
-import MenuPointer from '../layout/MenuPointer'
 
 export default function ProductDetail() {
   let { name } = useParams();
@@ -48,6 +47,11 @@ export default function ProductDetail() {
     { width: 600, itemsToShow: 4 },
     { width: 750, itemsToShow: 5 }
   ]
+  const [toggleState, setToggleState] = useState(1);
+
+  const toggleTab = (index) => {
+    setToggleState(index);
+  };
   return (
     <div>
       <p><span>&nbsp;{changeArray[0]}</span><span><Icon name='angle right' /></span>
@@ -103,8 +107,53 @@ export default function ProductDetail() {
             ))
           }
         </Carousel>
-        <MenuPointer/>
-        <Grid.Row columns={1}>
+        <div className="containerPointer">
+      <div className="bloc-tabsPointer">
+        <button
+          className={toggleState === 1 ? "tabsPointer active-tabsPointer" : "tabsPointer"}
+          onClick={() => toggleTab(1)}
+        >
+          Ürün Açıklaması
+        </button>
+        <button
+          className={toggleState === 2 ? "tabsPointer active-tabsPointer" : "tabsPointer"}
+          onClick={() => toggleTab(2)}
+        >
+          Teknik Özellikler
+        </button>
+        <button
+          className={toggleState === 3 ? "tabsPointer active-tabsPointer" : "tabsPointer"}
+          onClick={() => toggleTab(3)}
+        >
+          Yorumlar
+        </button>
+        <button
+          className={toggleState === 4 ? "tabsPointer active-tabsPointer" : "tabsPointer"}
+          onClick={() => toggleTab(4)}
+        >
+          Ödeme Seçenekleri
+        </button>
+        <button
+          className={toggleState === 5 ? "tabsPointer active-tabsPointer" : "tabsPointer"}
+          onClick={() => toggleTab(5)}
+        >
+          Teslimat ve İade
+        </button>
+        <button
+          className={toggleState === 6 ? "tabsPointer active-tabsPointer" : "tabsPointer"}
+          onClick={() => toggleTab(6)}
+        >
+          Hata Bildir
+        </button>
+      </div>
+
+      <div className="content-tabsPointer">
+        <div
+          className={toggleState === 1 ? "contentPointer  active-contentPointer" : "contentPointer"}
+        >
+          <h2>Ürün Açıklaması</h2>
+          <Grid>
+          <Grid.Row columns={1}>
           <Grid columns='two' divided>
             <Grid.Row>
               <Grid.Column>
@@ -129,6 +178,123 @@ export default function ProductDetail() {
             </Grid.Row>
           </Grid>
         </Grid.Row>
+        </Grid>
+        </div>
+
+        <div
+          className={toggleState === 2 ? "contentPointer  active-contentPointer" : "contentPointer"}
+        >
+          <h2>Teknik Özellikler</h2>
+          <p>
+          {product.productDetail}
+          </p>
+        </div>
+
+        <div
+          className={toggleState === 3 ? "contentPointer  active-contentPointer" : "contentPointer"}
+        >
+          <h2>Yorumlar</h2>
+          <p>
+            Yorumlar Güncellenmektedir...
+          </p>
+        </div>
+        <div
+          className={toggleState === 4 ? "contentPointer  active-contentPointer" : "contentPointer"}
+        >
+          <h2>Ödeme Seçenekleri</h2>
+          <p>
+            Kredi Kartı Ödeme Seçenekleri
+            Cep telefonlarında kurumsal ticari kartlara taksit imkanı
+
+
+            Peşin	4.699 TL	4.699 TL
+            Peşin	4.699 TL	4.699 TL
+            Alışveriş Kredisi Ödeme Seçenekleri
+
+
+
+
+            Fibabanka müşterisi olmasanız dahi alışverişinizi Fibabanka kredisi ile sonlandırabilirsiniz.
+            Aşağıdaki Taksit Seçeneklerinden size uygun olanı ileriki aşamalarda Krediniz onaylandıgı takdirde seçebilirsiniz.
+
+            Açıklama	Taksit Tutarı	Toplam Geri Ödeme
+            3 Taksit - Faizsiz Masrafsız	1.566,33 TL	4.699,00 TL
+            6 Taksit - Faizsiz Masrafsız	783,17 TL	4.699,00 TL
+            10 Taksit - Faizsiz Masrafsız	469,90 TL	4.699,00 TL
+            12 Taksit - Hayat Sigortalı %12	438,57 TL	5.262,88 TL
+            Fibabanka Kredisi Başvuru Adımları
+            Siparişi onayla butonuna tıkladığınızda Fibabanka sitesine yönlendirileceksiniz. Açılan sayfadaki formu doldurarak ön başvuruda bulunun.
+
+            Fibabanka müşterisi değilseniz, krediniz onaylandığında Fibabanka’dan tarafınıza SMS ile bir link gönderilecektir. 3 gün içerisinde bu linkten Fibabanka Mobil Uygulamasını indirip, “Müşteri Olmak İstiyorum” adımından görüntülü görüşme yaparak müşteri olup, evraklarınızı online olarak onaylayıp kredi kullanımınızı tamamlayabilirsiniz. Evraklarınızı onaylamanızın ardından ödemeniz tarafımıza aktarıldıktan sonra siparişiniz onaylanıp adresinize kargolanacaktır.
+
+            Fibabanka müşterisi olmanız durumunda, krediniz onaylandığında, kredi evraklarınızı online olarak onaylamanızın ardından ödemeniz tarafımıza aktarılacak ve siparişiniz onaylanıp adresinize kargolanacaktır.
+          </p>
+        </div>
+        <div
+          className={toggleState === 5 ? "contentPointer  active-contentPointer" : "contentPointer"}
+        >
+          <h2>Teslimat</h2>
+          <p>
+            TESLİMAT
+
+            Standart Teslimat;
+            Standart teslimat seçeneğinde siparişleriniz Kolay Gelsin,Yurtiçi ve Horoz Lojistik ile gönderilmektedir. Siparişiniz faturalandıktan sonra ortalama 3-4 iş gününde adresinize teslimatı sağlanmaktadır.Televizyon ve klima siparişleriniz ise Horoz Lojistik ile gönderilmektedir. Siparişinizi verirken, teslimat aşamasında Kolay Gelsin,Yurtiçi ya da Horoz Lojistik’i seçerek Standart Teslimat seçeneğinden faydalanabilirsiniz. 250 TL ve üzeri ürünlerde kargo ücretsizdir.
+
+
+            Hızlı Teslimat;
+            Hızlı teslimat seçeneğinde siparişleriniz Kolay Gelsin Kargo’yla gönderilmektedir. Siparişiniz faturalandıktan sonra ortalama 2 iş gününde adresinize teslimatı sağlanmaktadır. Siparişinizi verirken, teslimat aşamasında Kolay Gelsin Kargo’yu seçerek  (adresiniz uygunsa teslimat aşamasında çıkacaktır) Hızlı Teslimat seçeneğinden faydalanabilirsiniz. 250 TL ve üzeri ürünlerde kargo ücretsizdir. *Hızlı teslimattan yararlanmak için siparişini vereceğiniz ürünün stok durumunun uygun olması ve teslimat adresinin Kolay Gelsin Kargo’nun hizmet bölgesinde olması gerekmektedir. Kolay Gelsin’in teslimat yaptığı iller; Adana, Ankara, Antalya, Balıkesir, Bursa, Eskişehir, Gaziantep, Hatay, İstanbul, İzmir, Kayseri, Kırıkkale, Kocaeli, Konya, Manisa, Mersin, Muğla, Sakarya, Tekirdağ, Yalova
+
+            Mağaza Teslim;
+            Mağazadan teslim seçeneğinden faydalanabilmek için siparişinizi verirken, teslimat aşamasında Mağaza Teslim seçeneğini seçmeniz gerekmektedir. Siparişiniz tamamlandıktan sonra seçtiğiniz mağazamızdan hemen teslim alabilirsiniz.
+
+            *Mağaza teslim seçeneği yalnızca 3D Secure kullanılarak verilen siparişlerde geçerlidir, teslimat yalnızca kredi kartı sahibine yapılmaktadır.
+            7 iş günü içerisinde teslim alınmayan siparişler iptal edilerek ücret iadesi işlemi başlatılacaktır.
+
+            Siparişinizi mağaza teslim seçeneği ile vermek istiyorsanız aşağıdaki adımları uygulayabilirsiniz:
+
+            Satın almak istediğiniz ürünü sepete ekleyin ve Teslimat aşamasında gidin.
+            Teslimat aşamasının altındaki mağazadan teslim seçeneğini seçin.
+            Teslim almak istediğiniz mağazayı seçerek siparişinizi tamamlayın.
+            Siparişinizi verir vermez seçmiş olduğunuz mağazadan ürünü hemen teslim alabilirsiniz.
+
+
+
+
+            İADE VE İPTAL
+
+            Siparişiniz henüz faturalanmadıysa;
+            Hesabım Siparişlerim bölümünden sipariş iptal talebinizi iletebilirsiniz. Aynı zamanda 0850 222 56 56 numaralı çağrı merkezimizi arayıp dahili 1’i tuşlayarak ya da 0537 851 18 12 numaralı Whatsapp Hattımız’a iptal talebinizi iletebilirsiniz. İadeniz ortalama 1-2 iş gününde bankanıza bildirilmiş olacaktır.
+
+            Siparişiniz kargo sürecindeyse;
+            0850 222 56 56 numaralı çağrı merkezimizi arayıp dahili 1’i tuşlayarak ya da 0537 851 18 12 numaralı Whatsapp Hattımız’a iptal talebinizi iletebilirsiniz. Bu durumda gönderi tarafımızca kargodan geri çekilecektir. Ürün tarafımıza ulaştığında iade işlemleriniz başlatılacak ve ortalama 3 iş gününde iadeniz bankanıza bildirilecektir.
+
+            Siparişinizi teslim aldıysanız;
+            Cayma hakkınızı kullanmak için ürünü teslim aldıktan sonra 14 gün içerisinde aşağıdaki şekillerde tarafımıza başvurabilirsiniz.
+
+            0850 222 56 56 numaralı çağrı merkezimizi arayıp dahili 1’i tuşlayarak arayabilirsiniz.
+            0537 851 18 12 numaralı Whatsapp hattımıza yazabilirsiniz.
+            info@vatanbilgisayar.com adresimize e posta gönderebilirsiniz.
+            Size en yakın mağazamıza ürünü götürebilirsiniz.
+            İade ve iptal sayfasından kurye çağır butonunu kullanarak adresinize kurye çağırabilirsiniz. 1F176A iade koduyla tarafımıza ürünü gönderebilirsiniz.
+
+
+            *Cayma hakkınızı kullanmak istediğiniz ürünü tarafımıza gönderirken tüm kutu, aksesuar ve faturasıyla birlikte göndermeniz gerekmektedir.
+          </p>
+        </div>
+        <div
+          className={toggleState === 6 ? "contentPointer  active-contentPointer" : "contentPointer"}
+        >
+          <h2>Hata Bildir</h2>
+          <p>
+            Hatalı olduğunu düşündüğünüz ürün için bizimle iletişime geçebilirsiniz.
+            <Form>
+              <TextArea placeholder='Hatalı olduğunu düşündüğünüz ürün için bizimle iletişime geçebilirsiniz.' />
+              <Button>Gönder</Button>
+            </Form>
+          </p>
+        </div>
+      </div>
+    </div>
       </Grid>
 
     </div>
